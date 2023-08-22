08249ae9fb6d2f615e61c9ba002c20283d15e95b
143chaitu <142497555+143chaitu@users.noreply.github.com>
2023-08-22 20:45:30 +0530

    Add files via upload

---
+++ Arrayadd.java
@@ -0,0 +1,18 @@
+package javalearning;
+
+public class Arrayadd {
+
+	public static void main(String[] args) {
+		int []x={1,2,3,4};
+		int []y={1,2,3,4};
+		int []z=new int[4];
+		for(int i=0;i<=3;i++)
+		{
+			z[i]=x[i]+y[i];
+			System.out.print(z[i]+" ");// TODO Auto-generated method stub
+		}
+		
+
+	}
+
+}
